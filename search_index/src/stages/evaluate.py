5a1097c7454b7140cb1927087c210d35f3b8e7a1
AlexanderKolosov <alexckolosov@gmail.com>
2022-09-29 20:21:48 +0900

    Update metrics map@5: Add similarity category

--- search_index/src/stages/evaluate.py
+++ search_index/src/stages/evaluate.py
@@ -1,4 +1,5 @@
 import argparse
+from tkinter.tix import Tree
 import faiss
 import json
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ from tqdm import tqdm
 from typing import Text
 
 from src.utils.config import load_config
-from src.utils.metrics import calculate_map_per_query
+from src.utils.metrics import calculate_map_per_query, SimilarityType
 from search_index.src.utils import index_search, process_search_results
 
 
@@ -28,6 +29,13 @@ def evaluate(config_path: Text) -> None:
     PATH_TEST_DATA_EMBEDDINGS = f"{BASEDIR}/{DATASET_NAME}/embeddings/embeddings.parquet"
     PATH_INDEX = os.path.join(config.build_index.models_dir, config.build_index.model_name)
     PATH_DATASET_ANNOTATIONS = Path(f"{BASEDIR}/{DATASET_NAME}/annotations.csv")
+
+     # Save reports and log metrics  
+    PATH_REPORTS_DIR = Path(config.evaluate.reports_dir)
+    PATH_METRICS_ALL = PATH_REPORTS_DIR / config.evaluate.metrics_all
+    PATH_METRICS_MAP5_DIR = PATH_REPORTS_DIR / config.evaluate.metrics_map5
+    PATH_PLOT_METRICS_ALL = PATH_REPORTS_DIR / config.evaluate.plots_metrics_all
+    QUERY_RESULTS_PATH = PATH_REPORTS_DIR / config.evaluate.query_results
     
     # Load data 
     emb = pd.read_parquet(PATH_TEST_DATA_EMBEDDINGS)
@@ -39,13 +47,13 @@ def evaluate(config_path: Text) -> None:
     index = faiss.read_index(PATH_INDEX)
     
     # Build a query 
-    # (example: [{'id': 0, 'file': 'artwork/360.png', 'label': 0, 'label_name': 'artwork'}])
-    q_df = emb[['file', 'label', 'label_name']].reset_index().rename(columns={'index': 'id'})
-    query = [q_df.iloc[id].to_dict() for id in range(0, q_df.shape[0])]
+    # (example: [{'id': 0, 'file': 'artwork/360.png', 'label': 0, 'label_name': 'artwork', 'sim_category': 'close_positive'}])
+    q_df = emb[['file', 'label', 'label_name', 'sim_category']].reset_index().rename(columns={'index': 'id'})
+    queries = [q_df.iloc[id].to_dict() for id in range(0, q_df.shape[0])]
 
     # Run a search for the query 
     query_results = {}
-    for q in tqdm(query): 
+    for q in tqdm(queries): 
         s_dist, s_ids = index_search(index, embeddings[q.get('id')], k=5)
         q_results = process_search_results(q, s_dist,  s_ids, q_df)
         query_results.update({
@@ -56,25 +64,30 @@ def evaluate(config_path: Text) -> None:
             }
         })
 
-    # Prepare a report 
-    frames = []
-    for k in range(1, 6):
-        frames.append(calculate_map_per_query(query, query_results, k=k))
-    report = pd.concat(frames, axis=1)
-    report = report.rename(index=label_map_dict)  # rename label to label_name
-    report_map_at_5 = report['map@5'].to_dict()
-    report_map_at_5['map@5'] = report.mean()['map@5']
-    
-    print(report)
-    print(report_map_at_5)
-    
-    # Save reports and log metrics  
-    PATH_REPORTS_DIR = Path(config.evaluate.reports_dir)
-    PATH_METRICS_ALL = PATH_REPORTS_DIR / config.evaluate.metrics_all
-    PATH_METRICS_MAP5 = PATH_REPORTS_DIR / config.evaluate.metrics_map5
-    PATH_PLOT_METRICS_ALL = PATH_REPORTS_DIR / config.evaluate.plots_metrics_all
-    QUERY_RESULTS_PATH = PATH_REPORTS_DIR / config.evaluate.query_results
+    # Prepare a reports
+    os.makedirs(PATH_METRICS_MAP5_DIR, exist_ok=True)
 
+    for sim_type in SimilarityType:
+        
+        print(f'Similarity type: {sim_type}')
+        frames = []
+        
+        for k in range(1, 6):
+            frames.append(calculate_map_per_query(
+                queries, query_results, k=k, sim_type=sim_type
+            ))
+        
+        report = pd.concat(frames, axis=1)
+        report = report.rename(index=label_map_dict)  # rename label to label_name
+        report_map_at_5 = report['map@5'].to_dict()
+        report_map_at_5['map@5'] = report.mean()['map@5']
+
+        with open(PATH_METRICS_MAP5_DIR / f'{sim_type.value}.json' , 'w') as outfile:
+            json.dump(report_map_at_5, outfile, indent=4)
+    
+        print(report)
+        print(report_map_at_5)
+    
     query_items = []
 
     for query_id, query_info in query_results.items():
@@ -88,10 +101,7 @@ def evaluate(config_path: Text) -> None:
     query_results_df.to_csv(QUERY_RESULTS_PATH, index=False)
 
     report.to_csv(PATH_METRICS_ALL, index=True)
-    
-    with open(PATH_METRICS_MAP5 , 'w') as outfile:
-        json.dump(report_map_at_5, outfile)
-        
+
     f, ax = plt.subplots(figsize=(10, 6))
     ax = sns.heatmap(report.T, annot=True, cmap="YlGnBu")
     ax.set(
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,24 +1,64 @@
+from collections import defaultdict
+from enum import Enum
+from multiprocessing.pool import CLOSE
 import numpy as np
 import pandas as pd 
 from typing import Text, Dict
 
 
-def overall_precision(label, y_pred, k, n):
+class SimilarityType(Enum):
+    NO_SIMILARITY = 'no_similarity'
+    CLOSE_POSITIVE = 'close_positive'
+    CLOSE_POSITIVE_OR_HARD_POSITIVE = 'close_positive_or_hard_positive'
+
+
+def calc_rel(query, query_result, sim_type):
+
+    query_label = query['label']
+    query_result_label = query_result['label']
+    query_sim_cat = query['sim_category']
+    query_result_sim_cat = query_result['sim_category']
     
-    if len(y_pred) == 0: 
+    labels_equal = (query_label == query_result_label)
+    sim_cats_equal = (query_sim_cat and query_result_sim_cat)
+
+
+    if sim_type == SimilarityType.NO_SIMILARITY:
+        return labels_equal
+    
+    elif sim_type == SimilarityType.CLOSE_POSITIVE:
+        return (
+            labels_equal and
+            sim_cats_equal and
+            query_sim_cat == 'close_positive'
+        )
+    
+    elif sim_type == SimilarityType.CLOSE_POSITIVE_OR_HARD_POSITIVE:
+        return (
+            labels_equal and
+            sim_cats_equal and
+            (query_sim_cat in ['close_positive', 'hard_positive'])
+        )
+
+
+def overall_precision(
+    query, q_result, k, n,
+    sim_type: SimilarityType = SimilarityType.NO_SIMILARITY
+):
+    
+    if q_result.shape[0] == 0: 
         return 0.0
     
     j = min(n, k) 
-    # print(j)
-    
     ap_overall = []
     tp = 0
+
     for i in range(j):
-        rel = int(y_pred[i] == label)
+        # rel = int(y_pred[i] == label)
+        rel = calc_rel(query, q_result.iloc[i], sim_type)
         tp += rel
         ap = rel * tp / (i+1)
         ap_overall.append(ap)
-        # print(tp, rel, ap)
     
     return sum(ap_overall) / j
 
@@ -28,25 +68,29 @@ def map_per_query(labels, predictions, k, n):
     return np.mean([overall_precision(l, p, k, n) for l,p in zip(labels, predictions)])
 
 
-def calculate_map_per_query(query, query_results, k):
+def calculate_map_per_query(
+    queries, query_results, k, 
+    sim_type: SimilarityType = SimilarityType.NO_SIMILARITY
+):
     # Calculate MAP@k for each label in the query 
     
     # Count true labels for the query
-    query_n = {}
-    for q in query: 
-        true_label = q.get('label')
-        query_n[true_label] = query_n.get(true_label, 0) + 1
+    query_n = defaultdict(int)
+
+    for query in queries: 
+        true_label = query.get('label')
+        query_n[true_label] += 1
 
     # Calculate metrics for each label
     query_metrics = []
-    for q in query:
+    for query in queries:
         # print(f'query_results: {query_results}')
         # print(query_results[])
-        y_pred = pd.DataFrame(query_results.get(q.get('id')).get('results')).label
-        true_label = q.get('label')
+        q_result = pd.DataFrame(query_results.get(query.get('id')).get('results'))
+        true_label = query.get('label')
         n = query_n.get(true_label, 0)
 
-        q_map = overall_precision(true_label, y_pred, k, n)
+        q_map = overall_precision(query, q_result, k, n, sim_type)
         query_metrics.append({'label': true_label, f'map@{k}': q_map})
         
     return pd.DataFrame(query_metrics).groupby('label').agg('mean')
